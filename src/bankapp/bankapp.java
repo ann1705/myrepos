0d525c3dedafa49d7535dc1bc380b70e467f54cb
ann1705 <angiecardentedelossantos@gmail.com>
2025-08-10 21:59:28 +0800

    added register and login for clear version

--- src/bankapp/bankapp.java
+++ src/bankapp/bankapp.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 
 
 public class bankapp {
-
+    
+   
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
@@ -21,56 +22,118 @@ public class bankapp {
             
             System.out.print("Enter your choice: ");
             choice = sc.nextInt();
-            
-            if(choice < 1 || choice > 3) {
-                System.out.println("Invalid Selection! Try Again.");
-            }
-                } while(choice < 1 || choice > 3);
-        
-        
-        switch(choice){
-            
-            case 1:
-                
-                bankingClass bc = new bankingClass();
-                int attempts = 0;
-                
-                
-            while( attempts < 3) {
-                System.out.print("Enter Account No: ");
-                int accNo = sc.nextInt();
-                System.out.print("Enter your PIN: ");
-                int pin = sc.nextInt();
-                
-                if(bc.verifyAccount(accNo, pin)) {
-                    System.out.println("LOGIN SUCCES");
-                    break;
-                }else {
-                    attempts++;
-                    System.out.println("INVALID ACCOUNT!");
+            int resp = 0;  
+            switch(choice){
+                case 1:
+                  banking.bankingClass bApp[] = new bankingClass[10];
+                  bApp[0] = new bankingClass();
+                  do{
+                    System.out.print("1.Register Account");
+                    System.out.print("2. Login Account");
+                    System.out.print("3. View All Accounts");
+                    System.out.print("4. Enter Selection: ");
+                    int action = sc.nextInt();
                     
-                    if(attempts == 3){
-                        System.out.println("System Closed!");
-                        System.exit(0);
-                        
+                    
+                    switch(action){
+                        case 1: 
+                            System.out.println("Enter Account No.: ");
+                            bApp[0].setAccount(sc.nextInt());
+                            System.out.println("Enter Account PIN: ");
+                            bApp[0].setPin(sc.nextInt());
+                            System.out.println("Account registered successfully!");
+                            break;
+                            
+                        case 2:
+                            if(bApp[0].getAccountNo() == 0){
+                                System.out.println("No account registered yet! Please register first.");
+                                break;
+                            }
+                            
+                            int attempts = 3;
+                            
+                            System.out.print("Enter Account No.: ");
+                            int accNo = sc.nextInt();
+                            
+                            System.out.println("Enter Your PIN: ");
+                            int pin = sc.nextInt();
+                            
+                            while(!(bApp[0].verifyAccount(accNo, pin))){
+                                if(attempts == 1){
+                                    System.out.println("ATTEMPT LIMIT REACHED!");
+                                    System.exit(0);
+                                }
+                                
+                                attempts--;
+                                System.out.println("Attempts Left:"+attempts);
+                                
+                                System.out.println("INVALID ACCOUNT!");
+                                System.out.print("Enter Your Account No.:");
+                                accNo = sc.nextInt();
+                                System.out.print("Enter Your PIN: ");
+                                pin = sc.nextInt();
+                            }
+                            
+                            System.out.println("LOGIN SUCCESSFUL!");
+                            int bankChoice;
+                            do{
+                                System.out.println("1. Deposit");
+                                System.out.println("2. Withdraw");
+                                System.out.println("3. Check Balance");
+                                System.out.println("4. Exit");
+                                System.out.print("Enter your choice: ");
+                                bankChoice = sc.nextInt();
+                                
+                                switch(bankChoice){
+                                    case 1:
+                                        System.out.print("Enter amount to deposit: ");
+                                        double depositAmount = sc.nextDouble();
+                                        bApp[0].deposit(depositAmount);
+                                        System.out.println("Amount deposited successfully!");
+                                        break;
+                                        
+                                    case 2:
+                                        System.out.print("Enter amount to withdraw: ");
+                                        double withdrawAmount = sc.nextDouble();
+                                        bApp[0].withdraw(withdrawAmount);
+                                        break;
+                                        
+                                    case 3:
+                                        System.out.println("Current Balance: " + bApp[0].getBalance());
+                                        break;
+                                        
+                                    case 4:
+                                        System.out.println("Logging out...");
+                                        break;
+                                        
+                                    default:
+                                        System.out.println("Invalid choice!");
+                                        break;
+                                }
+                            }while(bankChoice != 4);
+                            break;
+                        case 3:
+                            break;
                     }
-                }
-            }
-            break;
-            
-            case 2: 
+                    
+                    System.out.println("Do you want to continue? (1/0):");
+                    resp = sc.nextInt();
+                }while(resp == 1);
+                
+                break;
+                
+            case 2:
+                
                 break;
                 
             case 3:
+                
                 break;
                 
             default:
-               
-              
-            
-                
-                
-        }
+                System.out.println("Invalid Selection!");
+                break;
+            }
+        }while(choice != 0);
     }
-    
 }
--- src/banking/bankingClass.java
+++ src/banking/bankingClass.java
@@ -5,25 +5,62 @@ package banking;
 public class bankingClass {
     
     
-    int accNo = 11111;
-    String name;
-    float availBalance;
-    int pin = 1234;
+    private int accountNo;
+    private double initialBalance;
+    private int pin;
     
+    public void setAccountNo(int accountNo) {
+        this.accountNo = accountNo;
+    }
+    public int getAccountNo() {
+        return accountNo;
+    }
+    public void setPin(int pin) {
+        this.pin = pin;
+    }
+    public int getPin() {
+        return pin;
+    }
     
-    public boolean verifyAccount(int acc, int pn) {
+    public boolean verifyAccount(int acc, int pn){
         
-        if(acc == accNo && pn == pin){
-            return true;
-        }else{
-            return false;
+            if(acc == accountNo && pn == pin){
+                return true;
+            }else{
+                return false;
+            }
+    }
+    
+    public void setAccount(int accountNo){
+        this.accountNo = accountNo;
+        this.initialBalance = 0.0; 
+    }
+    
+    public void viewBalance(){
+        System.out.println("Current Balance: " + initialBalance);
+    }
+    
+    public double getBalance() {
+        return initialBalance;
+    }
+    
+    public void withdraw(double withdrawAmount) {
+        if(withdrawAmount > 0 && withdrawAmount <= initialBalance){
+            initialBalance -= withdrawAmount;
+            System.out.println("Withdrawal successful! New balance: " + initialBalance);
+        } else if(withdrawAmount > initialBalance){
+            System.out.println("Insufficient balance!");
+        } else {
+            System.out.println("Invalid amount!");
         }
     }
     
- public int setAccount(){
-     return accNo;
- }   
-    public void viewBalance(){
-        
+    public void deposit(double depositAmount) {
+        if(depositAmount > 0){
+            initialBalance += depositAmount;
+            System.out.println("Deposit successful! New balance: " + initialBalance);
+        } else {
+            System.out.println("Invalid amount!");
+        }
     }
 }
